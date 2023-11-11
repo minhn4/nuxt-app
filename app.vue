5e973fd26c00b15355d8958d79ef00a13a14ae0b
Minh N <44449792+minhn4@users.noreply.github.com>
2023-11-11 11:24:10 +0700

    feat: add typescript (#4)

--- app.vue
+++ app.vue
@@ -34,18 +34,34 @@ const items = [
     },
   ],
 ];
+
+const links = [
+  {
+    label: "Profile",
+    avatar: {
+      src: "https://avatars.githubusercontent.com/u/739984?v=4",
+    },
+    badge: 100,
+  },
+  {
+    label: "Installation",
+    icon: "i-heroicons-home",
+    to: "/getting-started/installation",
+  },
+  {
+    label: "Vertical Navigation",
+    icon: "i-heroicons-chart-bar",
+    to: "/navigation/vertical-navigation",
+  },
+  {
+    label: "Command Palette",
+    icon: "i-heroicons-command-line",
+    to: "/navigation/command-palette",
+  },
+];
 </script>
 
 <template>
-  <UButton
-    icon="i-heroicons-pencil-square"
-    size="sm"
-    color="primary"
-    variant="solid"
-    label="Button"
-    :trailing="false"
-  />
-
   <UDropdown
     :items="items"
     :ui="{ item: { disabled: 'cursor-text select-text' } }"
@@ -71,4 +87,8 @@ const items = [
       />
     </template>
   </UDropdown>
+
+  <template>
+    <UVerticalNavigation :links="links" />
+  </template>
 </template>
