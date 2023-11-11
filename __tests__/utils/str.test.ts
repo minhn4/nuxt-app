aafce08ae92123e474f1a8907220a4bbbed45d50
Minh N <44449792+minhn4@users.noreply.github.com>
2023-11-11 16:17:47 +0700

    feat: add vitest (#7)

---
+++ __tests__/utils/str.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+
+import { toTitleCase } from "../../utils/str";
+
+describe("Title case unit test", () => {
+  test("Convert a string to title case", () => {
+    expect(toTitleCase("A")).toBe("A");
+    expect(toTitleCase("a")).toBe("A");
+    expect(toTitleCase("BRIAN")).toBe("Brian");
+    expect(toTitleCase("BRIAN brett")).toBe("Brian Brett");
+    expect(toTitleCase("ALBERTA'LYN SMITH")).toBe("Alberta'lyn Smith");
+    expect(toTitleCase("Alberta'lyn Smith")).toBe("Alberta'lyn Smith");
+    expect(toTitleCase("BRENDA SMITH BRAAM")).toBe("Brenda Smith Braam");
+    expect(toTitleCase("brenda smith braam")).toBe("Brenda Smith Braam");
+    expect(toTitleCase("Mr. BEan 123")).toBe("Mr. Bean 123");
+    expect(toTitleCase("Mr. BEan123")).toBe("Mr. Bean123");
+    expect(toTitleCase("123 Main ST")).toBe("123 Main St");
+    expect(toTitleCase("Mary-JAMES")).toBe("Mary-James");
+  });
+});
